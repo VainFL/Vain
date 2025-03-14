33b28e1b887a3def2484f1eaed88ffe279ace54a
Vain <timo.vollhardt@gmail.com>
2025-03-14 17:42:31 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,52 @@
+repeat task.wait() until game:IsLoaded()
+if shared.vain then shared.vain:Uninject() end
+
+-- why do exploits fail to implement anything correctly? Is it really that hard?
+if identifyexecutor then
+	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+		getgenv().setthreadidentity = nil
+	end
+end
+
+local vain
 local loadstring = function(...)
     local res, err = loadstring(...)
     if err then
         print("Error loading string: " .. err)
     end
     return res
+end
+
+local queue_on_teleport = queue_on_teleport or function() end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local cloneref = cloneref or function(obj)
+	return obj
+end
+
+local playersService = cloneref(game:GetService('Players'))
+
+local function downloadFile(path, func)
+    if not isfile(path) then
+        local suc, res = pcall(function()
+            return game:HttpGet('https://raw.githubusercontent.com/VainFL/Vain/'..readfile('vain/profiles/commit.txt')..'/'..select(1, path:gsub('vain/', '')), true)
+        end)
+        if not suc or res == '404: Not Found' then
+	    print('Failed to download: '..path)
+            error(res)
+        end
+        if path:find('.lua') then
+            -- Add watermark comment for future reference
+            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vain updates.\n'..res
+        end
+        writefile(path, res)
+    end
+    -- Return the file content, or execute the provided function on the file
+    return (func or readfile)(path)
 end
 
 local function finishLoading()
@@ -47,29 +90,15 @@ local function finishLoading()
 	end
 end
 
-local function downloadFile(path, func)
-    if not isfile(path) then
-        local suc, res = pcall(function()
-            return game:HttpGet('https://raw.githubusercontent.com/VainFL/Vain/'..readfile('vain/profiles/commit.txt')..'/'..select(1, path:gsub('vain/', '')), true)
-        end)
-        if not suc or res == '404: Not Found' then
-	    print('Failed to download: '..path)
-            error(res)
-        end
-        if path:find('.lua') then
-            -- Add watermark comment for future reference
-            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vain updates.\n'..res
-        end
-        writefile(path, res)
-    end
-    -- Return the file content, or execute the provided function on the file
-    return (func or readfile)(path)
+if not isfile('vain/profiles/gui.txt') then
+	writefile('vain/profiles/gui.txt', 'new')
 end
+local gui = readfile('vain/profiles/gui.txt')
 
--- Function to download and load the GUI script from GitHub
-
--- Now we can load the GUI script
-local vain = loadstring(downloadFile('vain/scripts/gui.lua'), 'gui')()
+if not isfolder('vain/assets/'..gui) then
+	makefolder('vain/assets/'..gui)
+end
+vain = loadstring(downloadFile('vain/scripts/gui.lua'), 'gui')()
 
 shared.vain = vain
 
