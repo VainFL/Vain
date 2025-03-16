6bc1ca5192bd41201b07aa0678421f3b9632c5dd
Vain <timo.vollhardt@gmail.com>
2025-03-16 09:47:23 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 if shared.vain then shared.vain:Uninject() end
 
--- why do exploits fail to implement anything correctly? Is it really that hard?
-if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-		getgenv().setthreadidentity = nil
-	end
-end
-
 local vain
 local loadstring = function(...)
     local res, err = loadstring(...)
@@ -49,47 +42,6 @@ local function downloadFile(path, func)
     return (func or readfile)(path)
 end
 
-local function finishLoading()
-	vain.Init = nil
-	vain:Load()
-	task.spawn(function()
-		repeat
-			vain:Save()
-			task.wait(10)
-		until not vain.Loaded
-	end)
-
-	local teleportedServers
-	vain:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.vainIndependent) then
-			teleportedServers = true
-			local teleportScript = [[
-				shared.vainreload = true
-				if shared.vainDeveloper then
-					loadstring(readfile('newvain/loader.lua'), 'loader')()
-				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/VainFL/Vain/'..readfile('vain/profiles/commit.txt')..'/loader.lua', true), 'loader')()
-				end
-			]]
-			if shared.vainDeveloper then
-				teleportScript = 'shared.vainDeveloper = true\n'..teleportScript
-			end
-			if shared.vainCustomProfile then
-				teleportScript = 'shared.vainCustomProfile = "'..shared.vainCustomProfile..'"\n'..teleportScript
-			end
-			vain:Save()
-			queue_on_teleport(teleportScript)
-		end
-	end))
-
-	if not shared.vainreload then
-		if not vain.Categories then return end
-		if vain.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vain:CreateNotification('Finished Loading', vain.vainButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vain.Keybind, ' + '):upper()..' to open GUI', 5)
-		end
-	end
-end
-
 if not isfile('vain/profiles/gui.txt') then
 	writefile('vain/profiles/gui.txt', 'new')
 end
@@ -98,26 +50,4 @@ local gui = readfile('vain/profiles/gui.txt')
 if not isfolder('vain/assets/'..gui) then
 	makefolder('vain/assets/'..gui)
 end
-vain = loadstring(downloadFile('vain/scripts/gui.lua'), 'gui')()
-
-shared.vain = vain
-
-if not shared.VainIndependent  then
-	loadstring(downloadFile('vain/scripts/universal.lua'), 'universal')()
-	if isfile('vain/scripts/bedwars.lua') then
-		loadstring(readfile('vain/scripts/bedwars.lua'), tostring(game.PlaceId))(...)
-	else
-		if not shared.VainDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/VainFL/Vain/'..readfile('vain/profiles/commit.txt')..'/scripts/bedwars.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('vain/scripts/bedwars.lua'), tostring(game.PlaceId))(...)
-			end
-		end
-	end
-	finishLoading()
-else
-	vain.Init = finishLoading
-	return vain
-end
+vain = loadstring(downloadFile('vain/scripts/gui.lua'))()
